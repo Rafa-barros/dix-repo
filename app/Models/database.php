09efc2bd811d96a84c57910403e27db41fea3be8
Vinicius-Venturini <viniciusventurini@estudante.ufscar.br>
2021-01-08 09:52:43 -0300

    model database finalizado

--- app/Models/database.php
+++ app/Models/database.php
@@ -19,5 +19,33 @@ class Database extends PDO{
 		$this->connection = new PDO("$this->db_sqlType:dbname=$this->db_name;host=$this->db_host;port=$this->db_port", "$this->db_user", "$this->db_pwd");
 	}
 
+	/*Essa função vai receber a query já preparada, e vai percorrer o array dos parâmetros setando cada variável
+	da requisição sql para o valor a ser utilizada.
+	$stmt é a query já pronta
+	$key é a variável da requisição sql
+	$value é o valor a ser utilizado na variável
+	*/
+	public function mountQuery($stmt, $parameters){
+		foreach($parameters as $key => $value){
+			$stmt->bindParam($key, $value);
+		}
+	}
 
+	/*Essa função recebe o pedido a ser feito para o banco de dados e chama a função mountQuery() para preparar
+	os parâmetros a serem usados na requisição.
+	$query é o pedido a ser feito
+	$parameters são as variáveis a serem utilizadas
+	*/
+	public function executeQuery(string $query, array $parameters = []){
+		$stmt = $this->connection->prepare($query);
+		$this->mountQuery($stmt, $parameters);
+		$verify = $stmt->execute();
+		if($verify == true){
+			return $stmt;
+		}else{
+			return false;
+		}
+	}
+
+}
 ?>

b959454f3ade1f7e9e097fc6d73d9e849bd969b0
Vinicius-Venturini <viniciusventurini@estudante.ufscar.br>
2021-01-10 23:15:41 -0300

    Criando arquivo de cookies, e iniciando sistema de login

--- app/Models/loginUsuario.php
+++ app/Models/loginUsuario.php
@@ -1,9 +1,43 @@
 <?php
 
-namespace App\Models;
+session_start();
+require ('database.php');
+require ('cookie.php');
 
-class loginUsuario {
-    public function registrar(){
-        echo ("Iniciando Registro");
-    }
+$email = isset($_POST["email"]) ? $_POST["email"] : "";
+$password = isset($_POST["pwd"]) ? $_POST["pwd"] : "";
+
+function verificaLogin($email, $password){
+
+	$conn = new Database();
+	$result = $conn->executeQuery('SELECT email, password, verifyEmail FROM  WHERE email = :EMAIL AND password = :PWD LIMIT 1', array(
+		':EMAIL' => $email,
+		':PWD' => $password
+	));
+
+	$res = $result->fetch();
+
+	if(empty($res)){
+		return FALSE;
+	}else{
+		foreach($res as $key => $value){
+			if($key == "2"){
+				if($value == 0){
+					return FALSE;
+				}else{
+					return TRUE;
+				}
+			}
+		}
+	}
+
 }
+
+if(verificaLogin($email, $password)){
+	$cookie = new cookie();
+	$cookie->newCookie($email);
+}else{
+
+}
+
+?>

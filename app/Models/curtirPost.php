97fc20d9b2e85782d629e2321dbc20967f99d921
marvinn2002 <marvinn2002@hotmail.com>
2021-01-28 15:19:22 -0300

    feat: sistema de likes

--- app/Models/curtirPost.php
+++ app/Models/curtirPost.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use App\Models\Database;
 use PDO;
 
-$idPost = $_POST['id'];
+$idPost = htmlentities($_POST['id']);
 $conn = new Database();
 
 //Encontra o id do usuário
@@ -15,3 +15,38 @@ $resultIdUser = $conn->executeQuery('SELECT id FROM users WHERE email = :EMAIL',
 $resultIdUser = $resultIdUser->fetch();
 $idUser = $resultIdUser['0'];
 
+$resultCond = $conn->executeQuery('SELECT idPost FROM assoc_users_likes WHERE idPost = :IDPOST AND idUser = :IDUSER', array(
+    ':IDPOST' => $idPost,
+    ':IDUSER' => $idUser
+));
+$resultCond = $resultCond->fetch();
+
+if (!(empty($resultCond))){
+    $codigo = 200;
+    //Insere o like do usuario no post
+    $conn->executeQuery('INSERT INTO assoc_users_likes (idPost, idUser) VALUES (:IDPOST, :IDUSER)', array(
+        ':IDPOST' => $idPost,
+        ':IDUSER' => $idUser
+    ));
+
+    //Adiciona um like
+    $conn->executeQuery('UPDATE posts SET likes=likes+1 WHERE id = :ID', array(
+        ':ID' => $idPost
+    ));
+} else {
+    $codigo = 201;
+    //Remove o like do usuario no post
+    $conn->executeQuery('DELETE FROM assoc_users_likes WHERE idPost = :IDPOST AND idUser = :IDUSER', array(
+        ':IDPOST' => $idPost,
+        ':IDUSER' => $idUser
+    ));
+
+    //Diminui um like
+    $conn->executeQuery('UPDATE posts SET likes=likes+1 WHERE id = :ID', array(
+        ':ID' => $idPost
+    ));
+}
+
+echo json_encode(array(
+    "codigo" => $codigo
+));

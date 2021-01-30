3f11927af8bfb07c4b097890474563e6e117f93b
CaioBrandini <brandinicaio@gmail.com>
2021-01-29 23:54:08 -0300

    feat: novas anomações chat

--- app/View/feed/chat.php
+++ app/View/feed/chat.php
@@ -28,7 +28,7 @@
                 <img class="foto-contato" src="https://scontent.fcpq4-1.fna.fbcdn.net/v/t1.0-9/130721573_2447514392060467_2518719859666796177_o.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=mYgZ_iaZMsgAX8OxWrV&_nc_ht=scontent.fcpq4-1.fna&oh=ebed84440b7f479a097acf165ec20a6f&oe=60368BEF" alt="foto de perfil">
                 <div class="contato-info">
                     <span class="contato-name">Vinícius Venturini</span>
-                    <p class="contact-last-message">iae</p>
+                    <p class="contact-last-message">Lorem, ipsum dolor.</p>
                 </div>
             </div>
             
@@ -36,30 +36,30 @@
                 <img class="foto-contato" src="https://scontent.fcpq4-1.fna.fbcdn.net/v/t1.0-9/48376083_1774848979290448_2227168575674646528_n.jpg?_nc_cat=103&ccb=2&_nc_sid=09cbfe&_nc_ohc=Cl4R69na_vQAX-XXNBd&_nc_ht=scontent.fcpq4-1.fna&oh=076a06f455381386ef3de739dec6d679&oe=6035D944" alt="foto de perfil">
                 <div class="contato-info">
                     <span class="contato-name">Marcus Vinícius</span>
-                    <p class="contact-last-message">iae</p>
+                    <p class="contact-last-message">Lorem ipsum dolor...</p>
                 </div>
             </div>
 
             <div class="contato">
                 <img class="foto-contato" src="/app/View/assets/css/img/Caio.jpg" alt="foto de perfil">
                 <div class="contato-info">
-                    <span class="contato-name">Caio Brandini</span>
-                    <p class="contact-last-message">iae</p>
+                    <span class="contato-name">Joana Paula</span>
+                    <p class="contact-last-message">Lorem.</p>
                 </div>
             </div>
             
             <div class="contato">
                 <img class="foto-contato" src="https://scontent.fcpq4-1.fna.fbcdn.net/v/t1.0-9/130721573_2447514392060467_2518719859666796177_o.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=mYgZ_iaZMsgAX8OxWrV&_nc_ht=scontent.fcpq4-1.fna&oh=ebed84440b7f479a097acf165ec20a6f&oe=60368BEF" alt="foto de perfil">
                 <div class="contato-info">
-                    <span class="contato-name">Vinícius Venturini</span>
-                    <p class="contact-last-message">iae</p>
+                    <span class="contato-name">Gabriel Costa</span>
+                    <p class="contact-last-message">Lorem, ipsum dolor.</p>
                 </div>
             </div>
             
             <div class="contato">
                 <img class="foto-contato" src="https://scontent.fcpq4-1.fna.fbcdn.net/v/t1.0-9/48376083_1774848979290448_2227168575674646528_n.jpg?_nc_cat=103&ccb=2&_nc_sid=09cbfe&_nc_ohc=Cl4R69na_vQAX-XXNBd&_nc_ht=scontent.fcpq4-1.fna&oh=076a06f455381386ef3de739dec6d679&oe=6035D944" alt="foto de perfil">
                 <div class="contato-info">
-                    <span class="contato-name">Marcus Vinícius</span>
+                    <span class="contato-name">Marcus souza</span>
                     <p class="contact-last-message">iae</p>
                 </div>
             </div>
@@ -67,7 +67,7 @@
             <div class="contato">
                 <img class="foto-contato" src="/app/View/assets/css/img/Caio.jpg" alt="foto de perfil">
                 <div class="contato-info">
-                    <span class="contato-name">Caio Brandini</span>
+                    <span class="contato-name">Viviane Soares</span>
                     <p class="contact-last-message">iae</p>
                 </div>
             </div>
@@ -75,7 +75,7 @@
             <div class="contato">
                 <img class="foto-contato" src="https://scontent.fcpq4-1.fna.fbcdn.net/v/t1.0-9/130721573_2447514392060467_2518719859666796177_o.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=mYgZ_iaZMsgAX8OxWrV&_nc_ht=scontent.fcpq4-1.fna&oh=ebed84440b7f479a097acf165ec20a6f&oe=60368BEF" alt="foto de perfil">
                 <div class="contato-info">
-                    <span class="contato-name">Vinícius Venturini</span>
+                    <span class="contato-name">Carlos augusto</span>
                     <p class="contact-last-message">iae</p>
                 </div>
             </div>
@@ -190,19 +190,19 @@
                 <!-- Mensagens -->
 
                 <div class="my-message">
-                    <div class="my-message-content">
+                    <div class="message-content">
                         <span>oe</span>
                     </div>
                 </div>
 
                 <div class="my-message">
-                    <div class="my-message-content">
+                    <div class="message-content">
                         <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span>
                     </div>
                 </div>
 
                 <div class="your-message">
-                    <div class="your-message-content">
+                    <div class="message-content">
                         <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span>
                     </div>
                 </div>
@@ -227,10 +227,24 @@
             $(".chat").hide();
     } 
 
+
+    //contato slecionado
     $(".contato").click(function(){
         let contato = $(this);
         let imgContato = contato.children(".foto-contato").attr("src");
         let nameContato = contato.children(".contato-info").children(".contato-name").text();
+        let listaContatos = $(".contato");
+
+
+        listaContatos.each(function(){
+            if($(this).css("background-color") == "rgb(245, 244, 244)"){
+                $(this).css("background-color", "rgb(252, 252, 252)");
+                return;
+            }
+        });
+
+        $(this).css("background-color", "rgb(245, 244, 244)");
+
         $(".chat-title-container").html('<img class="foto-perfil-contato" src='+imgContato+' alt="foto perfil contato"><p class="nome-contato-chat">'+nameContato+'</p>');
         $(".chat-title-container").show();
         $(".initial").hide();
@@ -239,6 +253,8 @@
 
     $(document).ready(function(e) {
     
+
+    //ao enviar mensagem
     $("form[ajax=true]").submit(function(e) {
         
         e.preventDefault();
@@ -253,15 +269,54 @@
             data: form_data,     
             cache: false,
             success: function(){
+
+                //envia menssagem
                 let msg = $(".form-control").val();
-                $(".chat-messages").append('<div class="my-message"><div class="my-message-content"><span>'+htmlEntities(msg)+'</span></div></div>');
-                $(".form-control").val("");            
+                $(".chat-messages").append('<div class="my-message"><div class="message-content"><span>'+htmlEntities(msg)+'</span></div></div>');
+
+
+                //apagar valor do submit
+                $(".form-control").val("");
+
+                let messages = $(".chat-messages").children();
+                let lastMessageDiv = messages[messages.length-1];
+
+                let lastMessage = $(".chat-messages").find(lastMessageDiv).find(".message-content span").text();
+
+                let listaContatos = $(".contato");
+
+                listaContatos.each(function(){
+                    if($(this).css("background-color") == "rgb(245, 244, 244)"){
+                        
+                        if(lastMessage.length > 16){
+                            lastMessage = lastMessage.slice(0,16) + '...';
+                        }
+
+                        $(this).find(".contato-info p").text(lastMessage);
+                        return;
+                    }
+                });
+                
+                //descer barra do chat
+                $(".chat-messages").scrollTop(100000);
+
+
+                //elevar contato
+                listaContatos.each(function(){
+                    if($(this).css("background-color") == "rgb(245, 244, 244)"){
+                        $(this).remove();
+                        $(".contact-list").prepend('<div class="contato" style="background-color: rgb(245, 244, 244)">'+$(this).html()+'</div>');
+                    }
+                });
+
+
             }           
         });    
+
+
+        
         }
 
-       
-        
     });
     
 });

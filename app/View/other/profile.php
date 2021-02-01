0193ddacd4f67cca5581c66acdd39b4421edebb2
CaioBrandini <brandinicaio@gmail.com>
2021-01-31 22:10:16 -0300

    fix: jquery profile

--- app/View/other/profile.php
+++ app/View/other/profile.php
@@ -1,3 +1,11 @@
+<nav class="nav">
+        <a class="a-logo-dix" href="/feed"><img class="logo-dix" src="/app/View/assets/css/img/logo_blue.png" alt="logo"></a> 
+        <div class="features-containers">
+            <a href="/feed"><i class="fas fa-home"></i></a> 
+            <a href="/chat"><i class="fas fa-comments"></i></a>
+            <a href=""><i class="fas fa-bell "></i></a>
+        </div>
+</nav>
 
 <div class="profile">
     <div class="profile-top">
@@ -34,8 +42,21 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
-        ...
+      <div class="modal-body" id="modal-body-vip">
+
+        <div class="modal-vip-container">
+            <div class="center">
+                <div>
+                    <h5 class="card-title mt-3">Assinatura mensal</h5>
+                    <ul class="mt-3 mb-3">
+                        <li>Todas as fotos liberadas</li>
+                        <li>Atualizações frequentes</li>
+                        <li></li>
+                    </ul>
+                    <a href="#" class="btn btn-warning mb-3" style="margin-left: 70px; color: #FAFAFA; font-weight: 500">15R$ / mês</a>
+                </div>
+            </div>
+        </div>
       </div>
       <div class="modal-footer">
       </div>
@@ -198,13 +219,10 @@
 </div>
 
 
-
+<script src="app/View/assets/js/post.js" class="deletar-1" ></script>
+<script src="app/View/assets/js/profile.js" class="deletar-2" ></script>
 
 <script>
-
-// $(".comment-input").on("change paste keyup", function() {
-//    if($(this).val()!="") $(this).css("background-color", "red");
-// });
 
 $('.comment-area').hide();
 
@@ -215,88 +233,69 @@ $('.comment-area').hide();
     } 
 
 
-// Botão de like
+//carregar posts
 
-    $('.btn-like').click(function (){
-        var likeIcon = document.querySelectorAll('.btn-like')[0];
+var flag = 1000;
+    $(document).scroll(function (e) {
+    var pos = $(this).scrollTop();
+    if (pos > flag){
 
-        if($(this).css("color") == "rgb(0, 0, 0)") $(this).css("color", "rgb(218, 51, 51)");
-        else $( this ).css( "color", "rgb(0, 0, 0)" );
+        // for(var i = 0; i < 5; i++){
+        //     $.ajax({
+        //         url:"app/Models/newPosts.php",
+        //         dataType: 'json',
+        //         type: "POST",
+        //         data: {email: "joatanzinho",
+        //             nameOp: "", //Dono do post
+        //             data: "", 
+        //             imgOp: "", //Imagem do dono do post
+        //             imgPost: "", //Imagem do post
+        //             postsVistos: postsVistosNav, 
+        //             descricao: "", 
+        //             likes: "",
+        //             liked: 0,
+        //             valor: 0,
+        //             gorjetas: 0,
+        //             idPost: 0,
+        //             qtdComentarios: 0
+        //         },
+        //         success:function(result){
+        //             $(".posts").append('<div class="card" id='+result.toString()+'> <div class="card-top"> <div class="l-card-top"> <a href=""><img class="profile-image" src="'+result.imgOp+'" alt="Foto perfil"></a> <div class="author-info"> <p class="author-name"> <a href="">+'result.nameOp'+</a></p><p class="post-time">'+result.data+' </p></div></div><div class="r-card-top"> <button type="button" class="btn btn-primary btn-card-follow">Seguir</button> </div></div><div class="midia-container"> <img src="'+result.imgPost+'" alt="Imagem do post"> </div><div class="card-bot"> <div class="post-status"> <span class="nlikes">'+result.likes+' curtidas</span> <span class="ncomments">'+result.qtdComentarios.toString()+' comentários </span> </div><div class="g-border"></div><div class="interactive"> <div class="like"> <button class="btn-like"><span><i style="color: unset" class="fas fa-fire-alt"></i> Curtir</span> </button> </div><div class="donate"> <button class="btn-donate" data-toggle="modal" data-target="#donate-modal"><i class="fas fa-coins"></i>Gorjeta</span></button> <span> </div><div class="comment"> <button class="btn-comment"><i class="far fa-comment"></i>Comentários</span></button><span> </div></div><div class="comment-area"> <div class="end-comment-show"></div><div class="comment-now"> <form method="POST" class="comment-form" ajax="true"> <input type="text" class="form-control comment-input" placeholder="Deixe seu comentário aqui..." required> <button class="btn-send-comment"> Enviar </button> </form> </div></div></div></div>');
 
-        var card = $(this).parents(".card")
-        text = card.find(".nlikes").text();
-        if($(this).css("color") == "rgb(0, 0, 0)"){
-        card.find(".nlikes").text((parseInt(text.split()[0])+ 1).toString() + " curtidas");
-        } 
-        else {card.find(".nlikes").text((parseInt(text.split()[0])- 1).toString() + " curtidas")}
+        //             postsVistosNav[postsVistosNav.length] = result.idPost;
+                    
+        //         },
+        //         error:function(req, status, error){
+        //             window.alert(req);
+        //             window.alert(status);
+        //             window.alert(error);
+        //         }
+        //         });
+        // }
 
-    });
-
-    $('.btn-comment').click(function (){
-        var card = $(this).parents(".card").find('.comment-area').show();
-    });
-
-
-// Comentário
-
-$(document).ready(function(e) {
-    
-    $("form[ajax=true]").submit(function(e) {
+        $(".profile-posts").append(' <div class="card"> <div class="card-top"> <div class="l-card-top"> <a href=""><img class="profile-image" src="/app/View/assets/css/img/Caio.jpg" alt="Foto perfil"></a> <div class="author-info"> <p class="author-name"> <a href="">Caio Brandini</a></p><p class="post-time"> 34 min </p></div></div><div class="r-card-top"> <button type="button" class="btn btn-primary btn-card-follow">Seguir</button> </div></div><div class="midia-container"> <img src="https://scontent.fcpq4-1.fna.fbcdn.net/v/t1.0-9/140726030_2932453123741462_2090715909532181379_n.jpg?_nc_cat=104&ccb=2&_nc_sid=730e14&_nc_ohc=OBII42r_vCIAX_KW_Ov&_nc_ht=scontent.fcpq4-1.fna&oh=c5e6105ef7a97f1ede98edc740351769&oe=602F52B8" alt=""> </div><div class="card-bot"> <div class="post-status"> <span class="nlikes">218 curtidas</span> <span class="ncomments">23 comentários</span> </div><div class="g-border"></div><div class="interactive"> <div class="like"> <button class="btn-like"><span><i style="color: unset" class="fas fa-fire-alt"></i> Curtir</span> </button> </div><div class="donate"> <button class="btn-donate" data-toggle="modal" data-target="#donate-modal"><i class="fas fa-coins"></i>Gorjeta</span></button><span> </div><div class="comment"> <button class="btn-comment"><i class="far fa-comment"></i>Comentários</span></button><span> </div></div><div class="comment-area"> <div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">Gustavo123</a></span> Lorem, ipsum dolor.</p></div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">Fernandoo</a></span> Lorem ipsum dolor sit.</p></div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">AnaJulia_50</a></span> Lorem, ipsum.</p></div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">Marcus13</a></span> Lorem ipsum dolor sit amet consectetur adipisicing.<p> </div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href=""> LauraaaSouza</a></span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates expedita labore quas possimus. Eaque, molestiae. Nesciunt vero minima assumenda voluptatem?</p></div><div class="end-comment-show"></div><div class="comment-now"> <form method="POST" class="comment-form" ajax="true"> <input type="text" class="form-control comment-input" placeholder="Deixe seu comentário aqui..." required> <button class="btn-send-comment"> Enviar </button> </form> </div></div></div></div><div class="card"> <div class="card-top"> <div class="l-card-top"> <a href=""><img class="profile-image" src="/app/View/assets/css/img/Caio.jpg" alt="Foto perfil"></a> <div class="author-info"> <p class="author-name"> <a href="">Caio Brandini</a></p><p class="post-time"> 34 min </p></div></div><div class="r-card-top"> <button type="button" class="btn btn-primary btn-card-follow">Seguir</button> </div></div><div class="midia-container"> <img src="https://scontent.fcpq4-1.fna.fbcdn.net/v/t1.0-9/141305301_3904500662933554_4794639086175181746_o.jpg?_nc_cat=1&ccb=2&_nc_sid=730e14&_nc_ohc=yqZx4VzPNUoAX-2Goue&_nc_ht=scontent.fcpq4-1.fna&oh=f56c81a3579170fbcf3217b6d150fb36&oe=60314113" alt=""> </div><div class="card-bot"> <div class="post-status"> <span class="nlikes">218 curtidas</span> <span class="ncomments">23 comentários</span> </div><div class="g-border"></div><div class="interactive"> <div class="like"> <button class="btn-like"><span><i style="color: unset" class="fas fa-fire-alt"></i> Curtir</span> </button> </div><div class="donate"> <button class="btn-donate" data-toggle="modal" data-target="#donate-modal"><i class="fas fa-coins"></i>Gorjeta</span></button><span> </div><div class="comment"> <button class="btn-comment"><i class="far fa-comment"></i>Comentários</span></button><span> </div></div><div class="comment-area"> <div class="a-comment"> <span class="a-username"><a href="">Gustavo123</a></span> <p class="r-comment">oioioi</p></div><div class="end-comment-show"></div><div class="comment-now"> <form method="POST" class="comment-form" ajax="true"> <input type="text" class="form-control comment-input" placeholder="Deixe seu comentário aqui..." required> <button class="btn-send-comment"> Enviar </button> </form> </div></div></div></div><div class="card"> <div class="card-top"> <div class="l-card-top"> <a href=""><img class="profile-image" src="/app/View/assets/css/img/Caio.jpg" alt="Foto perfil"></a> <div class="author-info"> <p class="author-name"> <a href="">Caio Brandini</a></p><p class="post-time"> 34 min </p></div></div><div class="r-card-top"> <button type="button" class="btn btn-primary btn-card-follow">Seguir</button> </div></div><div class="midia-container"> <img src="https://scontent.fcpq4-1.fna.fbcdn.net/v/t1.0-9/140726030_2932453123741462_2090715909532181379_n.jpg?_nc_cat=104&ccb=2&_nc_sid=730e14&_nc_ohc=OBII42r_vCIAX_KW_Ov&_nc_ht=scontent.fcpq4-1.fna&oh=c5e6105ef7a97f1ede98edc740351769&oe=602F52B8" alt=""> </div><div class="card-bot"> <div class="post-status"> <span class="nlikes">218 curtidas</span> <span class="ncomments">23 comentários</span> </div><div class="g-border"></div><div class="interactive"> <div class="like"> <button class="btn-like"><span><i style="color: unset" class="fas fa-fire-alt"></i> Curtir</span> </button> </div><div class="donate"> <button class="btn-donate" data-toggle="modal" data-target="#donate-modal"><i class="fas fa-coins"></i>Gorjeta</span></button><span> </div><div class="comment"> <button class="btn-comment"><i class="far fa-comment"></i>Comentários</span></button><span> </div></div><div class="comment-area"> <div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">Gustavo123</a></span> Lorem, ipsum dolor.</p></div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">Fernandoo</a></span> Lorem ipsum dolor sit.</p></div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">AnaJulia_50</a></span> Lorem, ipsum.</p></div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">Marcus13</a></span> Lorem ipsum dolor sit amet consectetur adipisicing.<p> </div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href=""> LauraaaSouza</a></span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates expedita labore quas possimus. Eaque, molestiae. Nesciunt vero minima assumenda voluptatem?</p></div><div class="end-comment-show"></div><div class="comment-now"> <form method="POST" class="comment-form" ajax="true"> <input type="text" class="form-control comment-input" placeholder="Deixe seu comentário aqui..." required> <button class="btn-send-comment"> Enviar </button> </form> </div></div></div></div><div class="card"> <div class="card-top"> <div class="l-card-top"> <a href=""><img class="profile-image" src="/app/View/assets/css/img/Caio.jpg" alt="Foto perfil"></a> <div class="author-info"> <p class="author-name"> <a href="">Caio Brandini</a></p><p class="post-time"> 34 min </p></div></div><div class="r-card-top"> <button type="button" class="btn btn-primary btn-card-follow">Seguir</button> </div></div><div class="midia-container"> <img src="https://scontent.fcpq4-1.fna.fbcdn.net/v/t1.0-9/141305301_3904500662933554_4794639086175181746_o.jpg?_nc_cat=1&ccb=2&_nc_sid=730e14&_nc_ohc=yqZx4VzPNUoAX-2Goue&_nc_ht=scontent.fcpq4-1.fna&oh=f56c81a3579170fbcf3217b6d150fb36&oe=60314113" alt=""> </div><div class="card-bot"> <div class="post-status"> <span class="nlikes">218 curtidas</span> <span class="ncomments">23 comentários</span> </div><div class="g-border"></div><div class="interactive"> <div class="like"> <button class="btn-like"><span><i style="color: unset" class="fas fa-fire-alt"></i> Curtir</span> </button> </div><div class="donate"> <button class="btn-donate" data-toggle="modal" data-target="#donate-modal"><i class="fas fa-coins"></i>Gorjeta</span></button><span> </div><div class="comment"> <button class="btn-comment"><i class="far fa-comment"></i>Comentários</span></button><span> </div></div><div class="comment-area"> <div class="a-comment"> <span class="a-username"><a href="">Gustavo123</a></span> <p class="r-comment">oioioi</p></div><div class="end-comment-show"></div><div class="comment-now"> <form method="POST" class="comment-form" ajax="true"> <input type="text" class="form-control comment-input" placeholder="Deixe seu comentário aqui..." required> <button class="btn-send-comment"> Enviar </button> </form> </div></div></div></div><div class="card"> <div class="card-top"> <div class="l-card-top"> <a href=""><img class="profile-image" src="/app/View/assets/css/img/Caio.jpg" alt="Foto perfil"></a> <div class="author-info"> <p class="author-name"> <a href="">Caio Brandini</a></p><p class="post-time"> 34 min </p></div></div><div class="r-card-top"> <button type="button" class="btn btn-primary btn-card-follow">Seguir</button> </div></div><div class="midia-container"> <img src="https://scontent.fcpq4-1.fna.fbcdn.net/v/t1.0-9/140726030_2932453123741462_2090715909532181379_n.jpg?_nc_cat=104&ccb=2&_nc_sid=730e14&_nc_ohc=OBII42r_vCIAX_KW_Ov&_nc_ht=scontent.fcpq4-1.fna&oh=c5e6105ef7a97f1ede98edc740351769&oe=602F52B8" alt=""> </div><div class="card-bot"> <div class="post-status"> <span class="nlikes">218 curtidas</span> <span class="ncomments">23 comentários</span> </div><div class="g-border"></div><div class="interactive"> <div class="like"> <button class="btn-like"><span><i style="color: unset" class="fas fa-fire-alt"></i> Curtir</span> </button> </div><div class="donate"> <button class="btn-donate" data-toggle="modal" data-target="#donate-modal"><i class="fas fa-coins"></i>Gorjeta</span></button><span> </div><div class="comment"> <button class="btn-comment"><i class="far fa-comment"></i>Comentários</span></button><span> </div></div><div class="comment-area"> <div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">Gustavo123</a></span> Lorem, ipsum dolor.</p></div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">Fernandoo</a></span> Lorem ipsum dolor sit.</p></div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">AnaJulia_50</a></span> Lorem, ipsum.</p></div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">Marcus13</a></span> Lorem ipsum dolor sit amet consectetur adipisicing.<p> </div><div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href=""> LauraaaSouza</a></span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates expedita labore quas possimus. Eaque, molestiae. Nesciunt vero minima assumenda voluptatem?</p></div><div class="end-comment-show"></div><div class="comment-now"> <form method="POST" class="comment-form" ajax="true"> <input type="text" class="form-control comment-input" placeholder="Deixe seu comentário aqui..." required> <button class="btn-send-comment"> Enviar </button> </form> </div></div></div></div>');
+        flag = flag += 2600;
+        load_js();
         
-        e.preventDefault();
-        
-        var form_data = $(this).serialize();
-        var form_url = $(this).attr("action");
-        
-        var card = $(this).parents(".card");
-        var commentInput = card.find(".comment-input").val();
-        var newComment = '<div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">CaioBrandini</a></span>'+ htmlEntities(commentInput) +'</p> </div>';
-
-        $.ajax({
-            url: form_url,    
-            data: form_data,     
-            cache: false,
-            success: function(){
-                var commentInput = card.find(".end-comment-show").before(newComment);
-                $('.comment-input').val("");      
-                ncomments = card.find(".ncomments").text();
-                card.find(".ncomments").text((parseInt(ncomments.split()[0])+ 1).toString() + " comentários")         
-            }           
-        });    
-        
-    });
-    
-});
-
-// Seguir
-
-$(".btn-profile-follow").click(follow);
-
-$(".btn-card-follow").click(follow);
-
-function follow() {
-    let thiss =  $(".btn-profile-follow");
-    if(thiss.css("background-color") != "rgb(57, 132, 218)"){
-        let nfollowers = $(".nfollowers").text();
-        thiss.css("background-color", "rgb(57, 132, 218)").css("color", "white").html('Seguindo <i class="fas fa-check check-follow-profile"></i>');
-        $(".btn-card-follow").css("background-color", "rgb(45, 81, 122))").html('Seguindo <i style="margin-left:6px;" class="fas fa-check"></i>');
-        $(".nfollowers").text((parseInt(nfollowers)+ 1).toString() );
     }
-    
-    else {
-        let nfollowers = $(".nfollowers").text();
-        thiss.css("background-color", "transparent").css("color", "rgb(57, 132, 218)").text("Seguir");
-        $(".btn-card-follow").css("background-color", "rgb(45, 81, 122))").html('seguir');
-        $(".nfollowers").text((parseInt(nfollowers)- 1).toString() );
+    });
+
+    function load_js() {
+      $(".deletar-1").remove();
+      $(".deletar-2").remove();
+
+      //Remover Jquery listeners (otimizar)
+      $("body").find("*").each(function() {
+        $(this).unbind();
+      });
+      var head= document.getElementsByTagName('head')[0];
+      var script= document.createElement('script');
+      script.src= 'app/View/assets/js/post.js';
+      head.appendChild(script).classList.toggle("deletar-1");
+      var script2= document.createElement('script');
+      script2.src= 'app/View/assets/js/profile.js';
+      head.appendChild(script2).classList.toggle("deletar-2");
     }
-}
-
-
-// HtmlEntities
-
-function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
 
 </script>
 

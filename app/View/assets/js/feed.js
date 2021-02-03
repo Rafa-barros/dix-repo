c61200c6f1e64a78c87247c7994ac66b70842392
CaioBrandini <brandinicaio@gmail.com>
2021-02-03 18:52:24 -0300

    fix: deixar de seguir (feed)

--- app/View/assets/js/feed.js
+++ app/View/assets/js/feed.js
@@ -1,18 +1,17 @@
 
-
 // Seguir
 
 $(".btn-card-follow").click(function(){
     if($(this).hasClass("unfollow")){
         //seguir
         $(this).css("background-color", "rgb(57, 132, 218)").css("color", "white").html('Seguindo <i class="fas fa-check check-follow-profile"></i>');
-        $(".btn-card-follow").css("background-color", "rgb(45, 81, 122))").html('Seguindo <i style="margin-left:6px;" class="fas fa-check"></i>').toggleClass("unfollow");
+        $(this).css("background-color", "rgb(45, 81, 122))").html('Seguindo <i style="margin-left:6px;" class="fas fa-check"></i>').toggleClass("unfollow");
     }
     
     else {
         //deixar de seguir
         $(this).css("background-color", "transparent").css("color", "rgb(57, 132, 218)").text("Seguir");
-        $(".btn-card-follow").css("background-color", "rgb(45, 81, 122))").html('seguir').toggleClass("unfollow");
+        $(this).css("background-color", "rgb(45, 81, 122))").html('seguir').toggleClass("unfollow");
         
     }
 });

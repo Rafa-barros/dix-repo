d095aa87bbab43b2537c150d914c3bb8743dc0a8
marvinn2002 <marvinn2002@hotmail.com>
2021-02-10 19:09:30 -0300

    feat: pegar dados do perfil

--- app/Controller/Profile.php
+++ app/Controller/Profile.php
@@ -3,9 +3,37 @@
 namespace App\Controller;
 
 class Profile {
+    public $urlMetodo;
+    public $id;
+    public $username;
+    public $birth;
+    public $pname;
+    public $typeuser;
+    public $posts;
+    public $img;
+    public $followers;
+    public $bio;
+
     public function index(){
-        echo ($this->urlMetodo);
-        //require("app/View/other/profile.php");
+        require("app/Models/getProfile.php");
+        $usuario = new ProfileModel();
+        $usuario->username = $this->urlMetodo;
+        $usuario->getInfo();
+
+        if (empty($usuario['id'])){
+            //require de uma view de uma pag q não existe
+        } else {
+            $this->id = $usuario['id'];
+            $this->username = $usuario['username'];
+            $this->birth = $usuario['birth'];
+            $this->pname = $usuario['pname'];
+            $this->typeuser = $usuario['typeuser'];
+            $this->posts = $usuario['posts'];
+            $this->img = $usuario['imgUser'];
+            $this->followers = $usuario['followers'];
+            $this->bio = $usuario['bio'];
+            require("app/View/other/profile.php");
+        }
     }
 
     public function carregarCSS(){
@@ -13,3 +41,4 @@ class Profile {
     }
 }
 
+$perfil = new Profile();

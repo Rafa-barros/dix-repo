fca21a425f70adda6813ba05b48b99a4887f2cff
Vinicius-Venturini <viniciusventurini@estudante.ufscar.br>
2021-02-10 16:26:09 -0300

    redirect cadastro fixed

--- app/Controller/Cadastro.php
+++ app/Controller/Cadastro.php
@@ -6,8 +6,17 @@ use App\Models\registroUsuario;
 
 class Cadastro {
     public function index(){
-        
         require('app/View/login/cadastro.php');
+        if(isset($_POST['submit'])){
+            $cadastro = new registroUsuario();
+            if($cadastro->verifyFields($_POST['email'], $_POST['pwd'], $_POST['confirmPwd'])){
+                $idVerify = $cadastro->newUser($_POST['email'], $_POST['pwd'], $_POST['username'], $_POST['dia'], $_POST['mes'], $_POST['ano'] , $_POST['pname']);
+                if($idVerify != FALSE){
+                    echo '<script type="text/javascript">window.location.href="http://dix.net.br/Verificarconta?id=' . $idVerify . '&email=' . $_POST['email'] . '"</script>';
+                    die();
+                }
+            }
+        }
     }
 
     public function carregarCSS() {
@@ -16,18 +25,3 @@ class Cadastro {
 }
 
 ?>
-
-<script type="text/javascript">
-    <?php
-        if(isset($_POST['submit'])){
-            $cadastro = new registroUsuario();
-            if($cadastro->verifyFields($_POST['email'], $_POST['pwd'], $_POST['confirmPwd'])){
-                $idVerify = $cadastro->newUser($_POST['email'], $_POST['pwd'], $_POST['username'], $_POST['dia'], $_POST['mes'], $_POST['ano'] , $_POST['pname']);
-                if($idVerify != FALSE){
-                    echo 'window.location.href="http://dix.net.br/Verificarconta?id="' . $idVerify . '"&email="' . htmlentities($_POST['email']);
-                    die();
-                }
-            }
-        }
-    ?>
-</script>

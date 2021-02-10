73f3d67b1a3487f5400c92c75487d06c349b68cf
Vinicius-Venturini <viniciusventurini@estudante.ufscar.br>
2021-02-10 15:55:39 -0300

    header location cadastro fixed

--- app/Controller/Cadastro.php
+++ app/Controller/Cadastro.php
@@ -1,19 +1,3 @@
-<script type="text/javascript">
-    <?php
-        if(isset($_POST['submit'])){
-            $cadastro = new registroUsuario();
-	    echo "aaa";
-            if($cadastro->verifyFields($_POST['email'], $_POST['pwd'], $_POST['confirmPwd'])){
-                $idVerify = $cadastro->newUser($_POST['email'], $_POST['pwd'], $_POST['username'], $_POST['dia'], $_POST['mes'], $_POST['ano'] , $_POST['pname']);
-                if($idVerify != FALSE){
-                    echo 'window.location.href="http://dix.net.br/VerificarConta?id="' . $idVerify . '"&email="' . htmlentities($_POST['email']);
-                    die();
-                }
-            }
-        }
-    ?>
-</script>
-
 <?php
 
 namespace App\Controller;
@@ -30,3 +14,20 @@ class Cadastro {
         echo("<link rel='stylesheet' href='app/View/assets/css/cadastro.css'>");
     }
 }
+
+?>
+
+<script type="text/javascript">
+    <?php
+        if(isset($_POST['submit'])){
+            $cadastro = new registroUsuario();
+            if($cadastro->verifyFields($_POST['email'], $_POST['pwd'], $_POST['confirmPwd'])){
+                $idVerify = $cadastro->newUser($_POST['email'], $_POST['pwd'], $_POST['username'], $_POST['dia'], $_POST['mes'], $_POST['ano'] , $_POST['pname']);
+                if($idVerify != FALSE){
+                    echo 'window.location.href="http://dix.net.br/Verificarconta?id="' . $idVerify . '"&email="' . htmlentities($_POST['email']);
+                    die();
+                }
+            }
+        }
+    ?>
+</script>

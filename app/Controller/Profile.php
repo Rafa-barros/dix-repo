419c82c316ec45500eb0389b9dc6bd8eef1845cb
marvinn2002 <marvinn2002@hotmail.com>
2021-02-10 19:17:05 -0300

    fix: profile e dados passados pro model

--- app/Controller/Profile.php
+++ app/Controller/Profile.php
@@ -3,36 +3,15 @@
 namespace App\Controller;
 
 class Profile {
-    public $urlMetodo;
-    public $id;
-    public $username;
-    public $birth;
-    public $pname;
-    public $typeuser;
-    public $posts;
-    public $img;
-    public $followers;
-    public $bio;
-
     public function index(){
         require("app/Models/getProfile.php");
+        echo ($this->urlMetodo);
         $usuario = new ProfileModel();
         $usuario->username = $this->urlMetodo;
         $usuario->getInfo();
 
         if (empty($usuario['id'])){
             //require de uma view de uma pag q não existe
-        } else {
-            $this->id = $usuario['id'];
-            $this->username = $usuario['username'];
-            $this->birth = $usuario['birth'];
-            $this->pname = $usuario['pname'];
-            $this->typeuser = $usuario['typeuser'];
-            $this->posts = $usuario['posts'];
-            $this->img = $usuario['imgUser'];
-            $this->followers = $usuario['followers'];
-            $this->bio = $usuario['bio'];
-            require("app/View/other/profile.php");
         }
     }
 
@@ -40,5 +19,3 @@ class Profile {
         echo ("<link rel='stylesheet' href='app/View/assets/css/profile.css'>");
     }
 }
-
-$perfil = new Profile();

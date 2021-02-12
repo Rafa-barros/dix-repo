5d0b60f511eeda8d5b4d6b332a48421f1101a5ea
Vinicius-Venturini <viniciusventurini@estudante.ufscar.br>
2021-02-12 15:51:51 -0300

    teste

--- app/Models/chatModel.php
+++ app/Models/chatModel.php
@@ -47,6 +47,7 @@ class chatModel{
 	}
 
 	public function carregarChats(){
+		$this->userId = intval($this->userId);
 		var_dump($this->userId);
 		$result = $this->conn->executeQuery('SELECT id FROM chats WHERE idUser = :ID OR idUser2 = :ID', array(
 			':ID' => $this->userId

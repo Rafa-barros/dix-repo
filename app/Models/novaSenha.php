99b938ddfc134197d4d1ca6ae530ffa4aa2151a7
Vinicius-Venturini <viniciusventurini@estudante.ufscar.br>
2021-02-13 15:47:55 -0300

    recuperar senha

--- app/Models/novaSenha.php
+++ app/Models/novaSenha.php
@@ -18,7 +18,7 @@ class novaSenha{
 	}
 
 	private function getUserId(){
-		$result = $conn->executeQuery('SELECT id FROM users WHERE email = :EMAIL', array(
+		$result = $conn->executeQuery('SELECT * FROM users WHERE email = :EMAIL', array(
 			':EMAIL' => $this->email
 		));
 		$result = $result->fetch();

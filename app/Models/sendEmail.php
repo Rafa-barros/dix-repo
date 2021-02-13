889876d8dd9c2e9916cb8482214e553502b5f733
Vinicius-Venturini <viniciusventurini@estudante.ufscar.br>
2021-02-13 14:58:50 -0300

    recuperar senha

--- app/Models/sendEmail.php
+++ app/Models/sendEmail.php
@@ -22,13 +22,13 @@ class sendEmail{
             $mail->Password   = 'dixadm78';
             $mail->SMTPSecure = 'ssl';
             $mail->Port       = 465;
-	    $mail->SMTPOptions = array(
-          	'ssl' => array(
-            		'verify_peer' => FALSE,
-            		'verify_peer_name' => FALSE,
-            		'allow_self_signed' => TRUE
-          	)
-	    );
+	        $mail->SMTPOptions = array(
+              	'ssl' => array(
+                		'verify_peer' => FALSE,
+                		'verify_peer_name' => FALSE,
+                		'allow_self_signed' => TRUE
+              	)
+    	    );
             $mail->CharSet    = 'UTF-8';
 
             //Recipients
@@ -43,6 +43,49 @@ class sendEmail{
                             Ou então clique aqui!<br>
                             <a href="http://dix.net.br/verificarconta?id=' . $id . '&email=' . $email . '&codigo=' . $codigo . '"><button>verificar</button></a></div>';
             $mail->AltBody = 'Para a Ativação de sua conta Dix, insira esse código ' . $codigo;
+
+            $mail->send();
+
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }   
+    }
+
+    public function mailSenha($email, $id, $codigo){
+        $mail = new PHPMailer(true);
+
+        try {
+
+            $mail->isSMTP();
+            $mail->SMTPDebug  = true;
+            $mail->do_debug   = 1;
+            $mail->Host       = 'dix.net.br';
+            $mail->SMTPAuth   = false;
+            $mail->Username   = 'admdix78@gmail.com';
+            $mail->Password   = 'dixadm78';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port       = 465;
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                        'verify_peer' => FALSE,
+                        'verify_peer_name' => FALSE,
+                        'allow_self_signed' => TRUE
+                )
+            );
+            $mail->CharSet    = 'UTF-8';
+
+            //Recipients
+            $mail->setFrom('admdix78@gmail.com');
+            $mail->addAddress($email);
+
+            // Content
+            $mail->isHTML(true);
+            $mail->Subject = 'Recuperação de senha Dix';
+            $mail->Body    = '<div class="jorge" style="background-color: #fafafa;">Para definir uma nova senha, insira esse código<br>
+                            ' . $codigo . '<br>
+                            Ou então clique aqui!<br>
+                            <a href="http://dix.net.br/recuperarsenha?id=' . $id . '&email=' . $email . '&codigo=' . $codigo . '"><button>verificar</button></a></div>';
+            $mail->AltBody = 'Para definir uma nova senha, insira esse código ' . $codigo;
 
             $mail->send();
 

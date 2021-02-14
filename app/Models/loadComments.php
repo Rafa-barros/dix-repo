16352ee7d449ab220c4aea69f3553731228e3acf
marvinn2002 <marvinn2002@hotmail.com>
2021-02-14 20:45:11 -0300

    fix: comentários em matriz

--- app/Models/loadComments.php
+++ app/Models/loadComments.php
@@ -13,13 +13,14 @@ $conn = new Database();
 $resultComment = $conn->executeQuery('SELECT * FROM comments WHERE idPost = :ID', array(
     ':ID' => $id
 ));
-
+$k = 0;
 while($row = $resultComment->fetch(PDO::FETCH_ASSOC)){
     $comentarios[$k][0] = $row['username'];
     $comentarios[$k][1] = $row['descript'];
     $comentarios[$k][2] = $row['likes'];
     $comentarios[$k][3] = $row['dateComment'];
     $comentarios[$k][4] = $row['comments'];
+    $k++;
 }
 
 echo json_encode(array (

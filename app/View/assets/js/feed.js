c2ca80307cd210659e1cd9102280819f8d120f79
CaioBrandini <brandinicaio@gmail.com>
2021-02-13 23:21:59 -0300

    fix: refatorando JS botao card follow do feed

--- app/View/assets/js/feed.js
+++ app/View/assets/js/feed.js
@@ -4,11 +4,7 @@
 $(document).on('click', '.btn-card-follow', function(){
 
     var targetName = $(this).closest('.card').find('.author-name a').text();
-
-    if($(this).hasClass("unfollow")){
-        //seguir
-        $(this).css("background-color", "rgb(57, 132, 218)").css("color", "white").html('Seguindo <i class="fas fa-check check-follow-profile"></i>');
-        $(this).css("background-color", "rgb(45, 81, 122))").html('Seguindo <i style="margin-left:6px;" class="fas fa-check"></i>').toggleClass("unfollow");
+    var btnFollow = $(this);
 
         // $.ajax({
         //     url: 'app/Models/follow.php', 
@@ -17,31 +13,19 @@ $(document).on('click', '.btn-card-follow', function(){
         //     data: {username: targetName},     
         //     cache: false,
         //     success: function(){
-        //         $(this).css("background-color", "rgb(57, 132, 218)").css("color", "white").html('Seguindo <i class="fas fa-check check-follow-profile"></i>');
-        //         $(this).css("background-color", "rgb(45, 81, 122))").html('Seguindo <i style="margin-left:6px;" class="fas fa-check"></i>').toggleClass("unfollow");
+                if(btnFollow.hasClass("unfollow")){
+        //         btnFollow.css("background-color", "rgb(57, 132, 218)").css("color", "white").html('Seguindo <i class="fas fa-check check-follow-profile"></i>');
+        //         btnFollow.css("background-color", "rgb(45, 81, 122))").html('Seguindo <i style="margin-left:6px;" class="fas fa-check"></i>').toggleClass("unfollow");
+                }
+
+                else {
+                    btnFollow.css("background-color", "transparent").css("color", "rgb(57, 132, 218)").text("Seguir");
+                    btnFollow.css("background-color", "rgb(45, 81, 122))").html('seguir').toggleClass("unfollow");
+                }
         //     }           
         // });   
 
-    }
-    
-    else {
-        //deixar de seguir
-        $(this).css("background-color", "transparent").css("color", "rgb(57, 132, 218)").text("Seguir");
-        $(this).css("background-color", "rgb(45, 81, 122))").html('seguir').toggleClass("unfollow");
 
-        // $.ajax({
-        //     url: 'app/Models/unfollow.php',   
-        //     dataType: 'json',
-        //     type: "POST", 
-        //     data: {username: targetName},     
-        //     cache: false,
-        //     success: function(){
-        //        $(this).css("background-color", "transparent").css("color", "rgb(57, 132, 218)").text("Seguir");
-        //        $(this).css("background-color", "rgb(45, 81, 122))").html('seguir').toggleClass("unfollow");
-        //     }           
-        // });    
-        
-    }
 });
 
 

fa6c9c05128e4662edef73fad94523e72323cffb
marvinn2002 <marvinn2002@hotmail.com>
2021-02-14 18:54:28 -0300

    fix: objeto pego da raiz no imagick

--- app/Models/createPost.php
+++ app/Models/createPost.php
@@ -63,9 +63,9 @@ $novoPost->uploadPost($media, (htmlentities($_POST['descriptPost'])), (htmlentit
 
 if ($extensao != 'mp4' && $extensao != 'avi' && $extensao != 'webp'){
     $imagemBorrada = new \Imagick($media);
-    $lock = new Imagick('media/lock.png');
+    $lock = new \Imagick('media/lock.png');
     $imagemBorrada->blurImage(35,35);
-    $imagemBorrada->compositeImage($lock, Imagick::COMPOSITE_DEFAULT, (((($imagemBorrada->getImageWidth()) - ($lock->getImageWidth())))/2), (((($imagemBorrada->getImageHeight()) - ($lock->getImageHeight())))/2));
+    $imagemBorrada->compositeImage($lock, \Imagick::COMPOSITE_DEFAULT, (((($imagemBorrada->getImageWidth()) - ($lock->getImageWidth())))/2), (((($imagemBorrada->getImageHeight()) - ($lock->getImageHeight())))/2));
     $imagemBorrada->writeImage('media' . '/' . (hash('haval128,5', $media)) . "." . $extensao);
 }
 

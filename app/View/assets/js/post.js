a60b8ee2b45cdc81e0647f0da475ab1a12f9b9b8
CaioBrandini <brandinicaio@gmail.com>
2021-02-15 01:13:35 -0300

    feat: nome ao comentar

--- app/View/assets/js/post.js
+++ app/View/assets/js/post.js
@@ -53,7 +53,7 @@ $(document).on('click', '.btn-comment', function(){
                 
                 if(result.comentarios != null && result.comentarios != undefined){
                     var i = result.comentarios.length - 1;
-                    
+
                     while (result.comentarios[i][0] !== ""){
                         commentArea.prepend(' <div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">'+result.comentarios[i][0]+'</a></span>'+result.comentarios[i][1]+'</p></div><div class="a-comment">');
                         if (i == 0){
@@ -84,7 +84,7 @@ $(document).ready(function(e) {
         var card = $(this).parents(".card");
         let idPost = card.attr('id');
         var commentInput = card.find(".comment-input").val();
-        var newComment = '<div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">CaioBrandini</a></span>'+ htmlEntities(commentInput) +'</p> </div>';
+        var newComment = '<div class="a-comment"> <p class="r-comment"> <span class="a-username"><a href="">'+ $('.features-containers a').text().split('/')[1] +'</a></span>'+ htmlEntities(commentInput) +'</p> </div>';
 
         $.ajax({
             url: 'app/Models/comentarPost.php',

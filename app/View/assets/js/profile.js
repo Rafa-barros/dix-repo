22860b459d7828b51689ef4c0098c595ec96eb5b
CaioBrandini <brandinicaio@gmail.com>
2021-02-16 18:02:45 -0300

    fix: AJAX junções de seguir

--- app/View/assets/js/profile.js
+++ app/View/assets/js/profile.js
@@ -48,23 +48,22 @@ function follow() {
         type: "POST",   
         data: {username: profuser},     
         cache: false,
-        success: function(){
-
-            if(!thiss.hasClass('seguindo')){
-                let nfollowers = $(".nfollowers").text();
-                thiss.css("background-color", "rgb(57, 132, 218)").css("color", "white").html('Seguindo <i class="fas fa-check check-follow-profile"></i>').addClass('seguindo');
-                $(".btn-card-follow").css("background-color", "rgb(45, 81, 122))").html('Seguindo <i style="margin-left:6px;" class="fas fa-check"></i>');
-                $(".nfollowers").text((parseInt(nfollowers)+ 1).toString() );
-            }
-            
-            else {
-                let nfollowers = $(".nfollowers").text();
-                thiss.css("background-color", "transparent").css("color", "rgb(57, 132, 218)").text("Seguir").removeClass('seguindo');
-                $(".btn-card-follow").css("background-color", "rgb(45, 81, 122))").html('seguir');
-                $(".nfollowers").text((parseInt(nfollowers)- 1).toString() );
-            }
-        }           
-    });   
+               
+    }).done(function(){
+        if(!thiss.hasClass('seguindo')){
+            let nfollowers = $(".nfollowers").text();
+            thiss.css("background-color", "rgb(57, 132, 218)").css("color", "white").html('Seguindo <i class="fas fa-check check-follow-profile"></i>').addClass('seguindo');
+            $(".btn-card-follow").css("background-color", "rgb(45, 81, 122))").html('Seguindo <i style="margin-left:6px;" class="fas fa-check"></i>');
+            $(".nfollowers").text((parseInt(nfollowers)+ 1).toString() );
+        }
+        
+        else {
+            let nfollowers = $(".nfollowers").text();
+            thiss.css("background-color", "transparent").css("color", "rgb(57, 132, 218)").text("Seguir").removeClass('seguindo');
+            $(".btn-card-follow").css("background-color", "rgb(45, 81, 122))").html('seguir');
+            $(".nfollowers").text((parseInt(nfollowers)- 1).toString() );
+        }
+    });
 
 
 }

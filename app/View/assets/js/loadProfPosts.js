c06061beecd8bedfdfc6993a99187170928ed769
CaioBrandini <brandinicaio@gmail.com>
2021-02-17 15:44:40 -0300

    feat: post pago class

--- app/View/assets/js/loadProfPosts.js
+++ app/View/assets/js/loadProfPosts.js
@@ -29,7 +29,7 @@ var flag = 300;
         //         success:function(result){
 
         //             var midia = '';
-
+        //             var pago = '';
         //             var postLiked = '';
                     
         //             if(result.liked == 1){
@@ -50,6 +50,8 @@ var flag = 300;
         //                 midia = '<video src="'+result.imgPost+'" controls controlslist="nodownload" style="max-width:100%;"></video>'
         //             }
 
+        //             if(result.valor > 0) pago = 'pago-' + result.valor.toString();
+
         //             var editBtn = '<div class="card-options-container" style="display: flex; justify-content: flex-end;"> <div class="btn-group dropup"> <button type="button" class="btn dropdown-toggle btn-edit-post" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="fas fa-ellipsis-h mr-4" style="font-size:22px; color:rgb(142, 142, 142,0.7); cursor: pointer;"></i> </button> <div class="post-edit-pop dropdown-menu "> <button class="dropdown-item edit-post-drop" type="button" data-toggle="modal" data-target="#edit-post" >Editar publicação</button> <button class="dropdown-item delete-post-drop" type="button">Apagar publicação</button> </div></div></div>';
 
         //             var cardFollowBtn = '<button type="button" class="btn btn-primary btn-card-follow">Seguindo <i style="margin-left:6px;" class="fas fa-check"></i> </button>';
@@ -57,7 +59,7 @@ var flag = 300;
         
                     // if(me == 0) editBtn = ' ';
                     // else cardFollowBtn =' ';
-        //             if(result.idPost != null) $(".profile-posts").append('<div class="card" id='+result.idPost.toString()+'> <div class="card-top"> <div class="top-card-top"> '+ editBtn +'<div class="l-card-top"> <a href="/profile/'+result.userOp+'"><img class="profile-image" src="'+$(".profile-img").attr('src')+'" alt="Foto perfil"></a> <div class="author-info"> <p class="author-name"> <a href="/profile/'+result.userOp+'">'+result.nameOp+'</a></p><p class="post-time"> '+ separaTempo(result.data)+' </p></div></div><div class="r-card-top"> '+ cardFollowBtn +' </div></div><div class="bot-card-top"> <pre class="description">'+result.descricao+'</pre></div></div><div class="midia-container"> '+ midia +' </div><div class="card-bot"> <div class="post-status"> <span class="nlikes">'+result.likes+' curtidas</span> <span class="ncomments">'+result.qtdComentarios.toString()+' comentários </span> </div><div class="g-border"></div><div class="interactive"> <div class="like"> <button class="btn-like" '+postLiked+'><span><i style="color: unset" class="fas fa-fire-alt"></i> Curtir</span> </button> </div><div class="donate"> <button class="btn-donate" data-toggle="modal" data-target="#donate-modal"><i class="fas fa-coins"></i>Gorjeta</span></button> <span> </div><div class="comment"> <button class="btn-comment"><i class="far fa-comment"></i>Comentários</span></button><span> </div></div><div class="comment-area"> <div class="end-comment-show"></div><div class="comment-now"> <form method="POST" class="comment-form" ajax="true"> <input type="text" class="form-control comment-input" placeholder="Deixe seu comentário aqui..." required> <button class="btn-send-comment"> Enviar </button> </form> </div></div></div></div>');
+        //             if(result.idPost != null) $(".profile-posts").append('<div class="card '+pago+'" id='+result.idPost.toString()+'> <div class="card-top"> <div class="top-card-top"> '+ editBtn +'<div class="l-card-top"> <a href="/profile/'+result.userOp+'"><img class="profile-image" src="'+$(".profile-img").attr('src')+'" alt="Foto perfil"></a> <div class="author-info"> <p class="author-name"> <a href="/profile/'+result.userOp+'">'+result.nameOp+'</a></p><p class="post-time"> '+ separaTempo(result.data)+' </p></div></div><div class="r-card-top"> '+ cardFollowBtn +' </div></div><div class="bot-card-top"> <pre class="description">'+result.descricao+'</pre></div></div><div class="midia-container"> '+ midia +' </div><div class="card-bot"> <div class="post-status"> <span class="nlikes">'+result.likes+' curtidas</span> <span class="ncomments">'+result.qtdComentarios.toString()+' comentários </span> </div><div class="g-border"></div><div class="interactive"> <div class="like"> <button class="btn-like" '+postLiked+'><span><i style="color: unset" class="fas fa-fire-alt"></i> Curtir</span> </button> </div><div class="donate"> <button class="btn-donate" data-toggle="modal" data-target="#donate-modal"><i class="fas fa-coins"></i>Gorjeta</span></button> <span> </div><div class="comment"> <button class="btn-comment"><i class="far fa-comment"></i>Comentários</span></button><span> </div></div><div class="comment-area"> <div class="end-comment-show"></div><div class="comment-now"> <form method="POST" class="comment-form" ajax="true"> <input type="text" class="form-control comment-input" placeholder="Deixe seu comentário aqui..." required> <button class="btn-send-comment"> Enviar </button> </form> </div></div></div></div>');
 
         //             if(result.likes == 1) { $("#"+result.idPost.toString()).find('.nlikes').text('1 curtida');}
 

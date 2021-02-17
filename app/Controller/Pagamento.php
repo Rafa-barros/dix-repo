18957b295c1695e56aa816fafd977b0febb9580e
marvinn2002 <marvinn2002@hotmail.com>
2021-02-17 20:40:58 -0300

    fix: verifica se o post existe ou o username existe

--- app/Controller/Pagamento.php
+++ app/Controller/Pagamento.php
@@ -17,11 +17,17 @@ class Pagamento {
 
     public function index(){
         $conn = new Database();
-        $condUser = $conn->executeQuery('SELECT id FROM users WHERE username = :USER', array(
-            ':USER' => htmlentities($_GET['user'])
-        ));
-        $condUser = $condUser->fetch();
-        if (isset($_GET['user']) && isset($_GET['amount']) && ($_GET['amount'] > 1) && (!empty($condUser))){
+        if (isset($_GET['idPost'])){
+            $condPost = $conn->executeQuery('SELECT id FROM posts WHERE id = :ID', array(
+                ':ID' => htmlentities($_GET['idPost'])
+            ));
+        } else {
+            $condUser = $conn->executeQuery('SELECT id FROM users WHERE username = :USER', array(
+                ':USER' => htmlentities($_GET['user'])
+            ));
+            $condUser = $condUser->fetch();
+        }
+        if (isset($_GET['user']) && isset($_GET['amount']) && ($_GET['amount'] > 1) && (!empty($condUser) || !empty($condPost))){
             require('app/Models/gerarTokenPS.php');
             $token = new \app\Models\Token();
             $url = "https://ws.pagseguro.uol.com.br/sessions";

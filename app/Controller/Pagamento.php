5db11025ea5f46621003f189c8b2cad86d9dd95a
marvinn2002 <marvinn2002@hotmail.com>
2021-02-17 22:45:52 -0300

    fix: sintaxe bandeira do cartão

--- app/Controller/Pagamento.php
+++ app/Controller/Pagamento.php
@@ -42,7 +42,7 @@ class Pagamento {
             if (isset($_POST['nomeTitular']) && isset($_POST['cpfTitular']) && isset($_POST['dddTel']) && isset($_POST['numeroTelefone']) && isset($_POST['email'])){
                 if (isset($_POST['nCartao']) && isset($_POST['cvv']) && isset($_POST['monthVal']) && isset($_POST['yearVal'])){
                     if (isset($_POST['rua']) && isset($_POST['nLocal']) && isset($_POST['bairro']) && isset($_POST['cep']) && isset($_POST['cidade']) && isset($_POST['estado'])){
-                        if (isset($_POST['senderHash']) && isset($_POST['brand'])){
+                        if (isset($_POST['senderHash']) && isset($_POST['bandeira'])){
                             if (isset($_POST['tokenCard'])){
                                 //Verifica se o preço do post condiz
                                 if (isset($_GET['idPost'])){

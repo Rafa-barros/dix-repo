880015bc992346fb690dc86dbd90008668dd4d44
Vinicius-Venturini <viniciusventurini@estudante.ufscar.br>
2021-02-18 18:46:39 -0300

    new posts fixed

--- app/View/assets/js/scrollInfinito.js
+++ app/View/assets/js/scrollInfinito.js
@@ -13,7 +13,7 @@ $(document).scroll(function (e){
     var pos = $(this).scrollTop();
     if (pos >= flag){
         newPost();
-        flag = flag + 3500;
+        flag = flag + 3000;
     
 }});
 

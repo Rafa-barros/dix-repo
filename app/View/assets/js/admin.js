0208ffda579daf61e7516ff96035c3c28eef2568
CaioBrandini <brandinicaio@gmail.com>
2021-02-19 07:45:19 -0300

    feat: text para zero

--- app/View/assets/js/admin.js
+++ app/View/assets/js/admin.js
@@ -3,7 +3,7 @@ var id = '';
 $(document).on('click', '.btn-pagar', function(){
     if(!($(this).hasClass('pago'))) $(this).closest('td').html('<button type="button" class="btn btn-outline-info pago">Pago</button>');
     id = $(this).attr('id');
-
+    $(this).closest('ganho').text('0.00');
     $.ajax({
         url: 'app/Models/zerarGanhos.php', 
         method: "POST",

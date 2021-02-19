04d1156e7fc311c0dbf519ddf6e908c27b675a60
marvinn2002 <marvinn2002@hotmail.com>
2021-02-19 03:39:44 -0300

    fix: segurança no painel admin

--- app/Controller/tLhWusnIpeENUtdAPIdrtEkQJiMbejVqWDMFIoZFbMQqwupR.php
+++ app/Controller/tLhWusnIpeENUtdAPIdrtEkQJiMbejVqWDMFIoZFbMQqwupR.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-class tLhWusnIpeENUtdAPIdrtEkQJiMbejVqWDMFIoZFbMQqwupR {
+class TLhWusnIpeENUtdAPIdrtEkQJiMbejVqWDMFIoZFbMQqwupR {
     public function index(){
         require("app/View/other/loginAdmin.php");
     }

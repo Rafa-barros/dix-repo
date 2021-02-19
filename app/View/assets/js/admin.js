70df9091798864cc0ecf5a680a66600e8a73ca6c
CaioBrandini <brandinicaio@gmail.com>
2021-02-19 07:06:54 -0300

    fix: Ajax pago

--- app/View/assets/js/admin.js
+++ app/View/assets/js/admin.js
@@ -1,7 +1,8 @@
 
+var id();
 $(document).on('click', '.btn-pagar', function(){
     if(!($(this).hasClass('pago'))) $(this).closest('td').html('<button type="button" class="btn btn-outline-info pago">Pago</button>');
-    var id = $(this).closest('th').text();
+    var id = $(this).closest('tr').find('th').text();
 
     $.ajax({
         url: 'app/Models/zerarGanhos.php', 

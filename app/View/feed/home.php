62d6bc36c1f8e8b06dfb799e971e5393439f2753
marvinn2002 <marvinn2002@hotmail.com>
2021-02-19 09:58:04 -0300

    fix: diretorio certo no profile

--- app/View/feed/home.php
+++ app/View/feed/home.php
@@ -108,7 +108,7 @@
                                 <input type="number" step="0.01" minlength="1" name="valor" class="form-control post-price" aria-label="Amount (to the nearest dollar)" placeholder="Preço">
                             </div>
                             <?php } else { ?>
-                                <h6 style=" font-weight: 300; margin-top: 40px;">Ganhe dinheiro com suas publicações <a href="<?php echo("https://dix.net.br/redirectInfluencer.php?name=" . $username); ?>" style=" color: #007bff;">tornando-se um influencer!</a></h6>
+                                <h6 style=" font-weight: 300; margin-top: 40px;">Ganhe dinheiro com suas publicações <a href="<?php echo("https://dix.net.br/redirectInfluencer?name=" . $username); ?>" style=" color: #007bff;">tornando-se um influencer!</a></h6>
                             <?php } ?>
                         </div>
                         <div class="loading-new-post"></div>

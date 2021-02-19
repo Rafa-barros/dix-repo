698ba223d8357d71d1a7366b26058c7ed3162056
Vinicius-Venturini <viniciusventurini@estudante.ufscar.br>
2021-02-19 01:00:37 -0300

    facebook auth fixed

--- app/Models/facebookAuth.php
+++ app/Models/facebookAuth.php
@@ -32,11 +32,11 @@ if(isset($_GET['code']) && isset($_GET['state'])){
 	if(isset($facebook_user_info['email'])){
 		if($registerFacebookAuth->verificaEmail($facebook_user_info['email'])){
 			$registerFacebookAuth->newUserAuth(htmlentities($facebook_user_info['email']), htmlentities($facebook_user_info['name']), $facebook_user_info['id']);
-			header("Location: /home");
+			header("Location: https://dix.net.br");
 		}else{
 			$loginFacebookAuth = new App\Models\loginUsuario();
 			$loginFacebookAuth->loginAuth($facebook_user_info['email'], $facebook_user_info['id']);
-			header("Location: /home");
+			header("Location: https://dix.net.br");
 		}
 	}
 }
